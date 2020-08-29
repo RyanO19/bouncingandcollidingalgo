ddf75b884c41f9b28bc630c739a3c6fef20bbd17
RyanO19 <70152778+RyanO19@users.noreply.github.com>
2020-08-29 11:55:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bouncingandcollidingalgo
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -5,10 +5,28 @@ function setup() {
   createCanvas(800,400);
  rect1 =  createSprite(400, 200, 50, 80);
  rect2 = createSprite(0,0, 80, 50)
+ rect1.shapeColor="green"
+ rect2.shapeColor="green"
+ rect1.debug=true;
+ rect2.debug=true;
 }
 
 function draw() {
   background(255,255,255);  
+  rect2.x=world.mouseX;
+  rect2.y=world.mouseY;
+  if(rect2.x - rect1.x < rect2.width/2 + rect1.width/2 && rect1.x - rect2.x < rect2.width/2 + rect1.width/2)
+  {
+    rect1.shapeColor="red"
+    rect2.shapeColor="red"
+  }
+
+  if(rect2.y - rect1.y < rect2.height/2 + rect1.height/2 && rect1.y - rect2.y < rect2.height/2 + rect1.height/2) 
+  {
+    rect1.shapeColor="red"
+    rect2.shapeColor="red"
+  }
   drawSprites();
 }
+
 
